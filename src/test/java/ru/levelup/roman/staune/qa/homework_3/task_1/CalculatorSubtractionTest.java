92634831f609085e805ed7a39278dba113054742
ragnards <mail>
2019-12-06 16:40:42 +0300

    добавлены тесты

--- src/test/java/ru/levelup/roman/staune/qa/homework_3/task_1/CalculatorSubtractionTest.java
+++ src/test/java/ru/levelup/roman/staune/qa/homework_3/task_1/CalculatorSubtractionTest.java
@@ -5,34 +5,34 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class CalculatorSummationTest {
+public class CalculatorSubtractionTest {
     private Calculator calculator = new Calculator();
 
     @DataProvider
     public Object[][] longOperands() {
         return new Object[][]{
-                {3, 5, 8},
-                {-3, -5, -8}
+                {3, 5, -2},
+                {5, 3, 2}
         };
     }
 
     @DataProvider
     public Object[][] doubleOperands() {
         return new Object[][]{
-                {3.3, 5.5, 8.8},
-                {-3.3, -5.5, -8.8}
+                {3.3, 5.5, -2.2},
+                {3.3, -5.5, 8.8}
         };
     }
 
     @Test(dataProvider = "longOperands", groups = "sum_sub_tests")
-    public void testSummationLongOperands(long firstNum, long secondNum, long expected) {
-        long actual = calculator.sum(firstNum, secondNum);
+    public void testSubtractionLongOperands(long firstNum, long secondNum, long expected) {
+        long actual = calculator.sub(firstNum, secondNum);
         Assert.assertEquals(actual, expected);
     }
 
     @Test(dataProvider = "doubleOperands", groups = "sum_sub_tests")
-    public void testSummationDoubleOperands(double firstNum, double secondNum, double expected) {
-        long actual = calculator.sum(firstNum, secondNum);
+    public void testSubtractionDoubleOperands(double firstNum, double secondNum, double expected) {
+        long actual = calculator.sub(firstNum, secondNum);
         Assert.assertEquals(actual, expected);
     }
 }
