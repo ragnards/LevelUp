287500c75cd0a1d2a93025e3322a012b198cfdd2
ragnards <mail>
2019-12-17 00:06:04 +0300

    добавлен ui тест на имя пользователя

--- src/main/java/ru/levelup/roman/staune/qa/homework_4/task_1/UserNameTest.java
+++ src/main/java/ru/levelup/roman/staune/qa/homework_4/task_1/UserNameTest.java
@@ -3,13 +3,14 @@ package ru.levelup.roman.staune.qa.homework_4.task_1;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class SiteAndUserNameTest {
+public class UserNameTest {
     private WebDriver driver;
 
     @BeforeClass
@@ -26,14 +27,22 @@ public class SiteAndUserNameTest {
         // 2. Проверить название сайта
         Assert.assertEquals(driver.getTitle(), "Users", "Название сайта не 'Users'");
         // 3. Войти на сайт
-        driver.findElement(By.partialLinkText("login")).click();
-        driver.findElement(By.name("input")).sendKeys("krev@et.ka");
+        driver.findElement(By.cssSelector("a[href*='login']")).click();
+        driver.findElement(By.name("login")).sendKeys("krev@et.ka");
         driver.findElement(By.xpath("//p[@class='lead']/..//input[@name='password']")).sendKeys("krevetka");
         driver.findElement(By.xpath("//p[@class='lead']/..//input[@type='submit']")).click();
+        // 4. Проверить имя пользователя в правом верхнем углу
+        WebElement userButton = driver.findElement(By.cssSelector(".dropdown-toggle"));
+        Assert.assertEquals(userButton.getText(), "Креветка",
+                "Имя пользователя не соответствует имени созданного пользователя");
+        // 5. Выполнить выход из системы
+        userButton.click();
+        driver.findElement(By.cssSelector("a[href *= 'logout']")).click();
     }
 
     @AfterClass
     public void tearDown() {
+        // 6. Закрыть браузер
         driver.close();
     }
 }
